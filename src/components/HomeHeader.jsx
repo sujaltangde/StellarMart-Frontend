3bfdd75d34f90df15048a4538bf7b11a3f0bf3b1
Sujal Tangde <sujaltagade01@gmail.com>
2023-08-25 11:29:56 +0530

    issues fixed

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { IoIosArrowDown } from 'react-icons/Io'
+import { IoIosArrowDown } from 'react-icons/io'
 import { Link as LinkSr } from 'react-scroll'
 import  Search  from './Search'
 
